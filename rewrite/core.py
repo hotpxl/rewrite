3a5d5cd57e2df097e4c2876cd1648323246de8c7
Yutian Li <hotpxless@gmail.com>
2017-07-15 19:52:04 -0700

    [master] Use `_reentrance` to prevent recursive rewriting.

--- rewrite/core.py
+++ rewrite/core.py
@@ -8,21 +8,20 @@ import types
 import inspect
 import functools
 
+_reentrance = False
+
 
 def rewrite(**rewrite_kwargs):
     def wrap(func):
+        global _reentrance
+        if _reentrance:
+            return func
+        _reentrance = True
         source_code = inspect.getsource(func)
         global_namespace = func.__globals__.copy()
         func_ast = ast.parse(source_code, mode='exec')
         func_ast.body[0].name += '_rewritten'
         new_name = func_ast.body[0].name
-
-        # Delete the decorator itself. Assume the attribute name is "rewrite".
-        for index, i in enumerate(func_ast.body[0].decorator_list):
-            if isinstance(i, ast.Call) and isinstance(
-                    i.func, ast.Attribute) and i.func.attr == 'rewrite':
-                del func_ast.body[0].decorator_list[index]
-                break
 
         if 'call_advice' in rewrite_kwargs:
             global_namespace['__call_advice'] = rewrite_kwargs['call_advice']
@@ -32,17 +31,22 @@ def rewrite(**rewrite_kwargs):
                     for i in ast.iter_child_nodes(node):
                         self.visit(i)
                     node.args.insert(0, node.func)
+                    node.args.insert(1, ast.Num(node.lineno))
+                    node.args.insert(2, ast.Num(node.col_offset))
                     node.func = ast.Name(id='__call_advice', ctx=ast.Load())
                     return node
 
             for i in func_ast.body[0].body:
                 Visitor().visit(i)
         ast.fix_missing_locations(func_ast)
+        # print(ast.dump(func_ast))
         exec(
             compile(func_ast, filename='<ast>', mode='exec'), global_namespace)
 
         @functools.wraps(func)
         def wrapped_func(*args, **kwargs):
+            global _reentrance
+            _reentrance = True
             nonlocal func_ast
             ret = global_namespace[new_name](*args, **kwargs)
             if 'post_function_hook' in rewrite_kwargs:
@@ -52,8 +56,10 @@ def rewrite(**rewrite_kwargs):
                     exec(
                         compile(func_ast, filename='<ast>', mode='exec'),
                         global_namespace)
+            _reentrance = False
             return ret
 
+        _reentrance = False
         return wrapped_func
 
     return wrap
--- test/1.py
+++ test/1.py
@@ -12,6 +12,7 @@ import numpy as np
 def other_stuff(func):
     def wrapped_fn():
         return func()
+
     return wrapped_fn
 
 
@@ -19,8 +20,8 @@ def some_other(func):
     return func
 
 
-def haha(f, *args, **kwargs):
-    print('in haha', f)
+def haha(f, lineno, col_offset, *args, **kwargs):
+    print('in haha', f, lineno, col_offset)
     return f(*args, **kwargs)
 
 
@@ -49,8 +50,14 @@ def func1():
     return b
 
 
-print(func1())
-print(func1())
+func1()
+
+# class AA():
+#     @rewrite.rewrite(call_advice=haha)
+#     def ak(self, h):
+#         print(h)
+#
+# AA().ak(23)
 
 # @rewrite.rewrite(call_advice=123)
 # @some_other
